4f8ad1cdea2b81fd9de9964a9a29b84a41342926
Uzoziealpha <uzozie.obi@icloud.com>
2021-09-12 15:01:24 +0800

    Validation form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,18 @@ class PostsController < ApplicationController
     end
 
     def new
+      @post = Post.new
     end 
 
     def create
        @post = Post.new(post_params)
-       @post.save
 
-       redirect_to @post
-    end
+       if @post.save
+         redirect_to @post
+       else
+         render 'new'
+       end
+      end   
 
     def show
        @post = Post.find(params[:id])
